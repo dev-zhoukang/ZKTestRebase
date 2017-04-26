1a54c526ee8f76b66ea1c36d8bb20260827ca084
ZK <dev.zhoukang@gmail.com>
2017-04-26 17:23:55 +0800

    v2 udpate

---
+++ ZKTestRebase/ZKTestRebase/ZKTestV2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZKTestV2ViewController.h
+//  ZKTestRebase
+//
+//  Created by ZK on 2017/4/26.
+//  Copyright © 2017年 ZK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZKTestV2ViewController : UIViewController
+
+@end
---
+++ ZKTestRebase/ZKTestRebase/ZKTestV2ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ZKTestV2ViewController.m
+//  ZKTestRebase
+//
+//  Created by ZK on 2017/4/26.
+//  Copyright © 2017年 ZK. All rights reserved.
+//
+
+#import "ZKTestV2ViewController.h"
+
+@interface ZKTestV2ViewController ()
+
+@end
+
+@implementation ZKTestV2ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
